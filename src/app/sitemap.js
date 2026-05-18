4bb038a0d6b974871955641394eaae117d74de72
tobymiles <ekwealortobechukwu22@gmail.com>
2026-05-18 23:43:12 +0100

    Added SEO

---
+++ src/app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.switchguardadvisory.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.switchguardadvisory.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.switchguardadvisory.com/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.switchguardadvisory.com/servicepage",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.switchguardadvisory.com/products",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.switchguardadvisory.com/founder",
+      lastModified: new Date(),
+    },
+  ];
+}
